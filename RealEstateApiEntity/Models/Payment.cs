d48d3c5cd0455c1669f4b4ddfd30e5019beb58ac
yigitumretastan <yigitumretastan.dev@gmail.com>
2025-08-08 14:05:49 +0300

    Added ListingId to Improve Payment Section

--- RealEstateApiEntity/Models/Payment.cs
+++ RealEstateApiEntity/Models/Payment.cs
@@ -8,7 +8,11 @@ namespace RealEstateApiEntity.Models
     public class Payment
     {
         public int Id { get; set; }
+        public int ListingId { get; set; }
         public required string PaymentMethod { get; set; }
-        public decimal Price { get; set; }
+        public required string CardName { get; set; }
+        public required int CardNumber { get; set; }
+        public required int CardCode { get; set; }
+        public decimal Price { get; }
     }
 }
